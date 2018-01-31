6a9eb60c7c9900f00667c0ad0850e7ff4d810b7d
alexgriff <alexalexgriffith@gmail.com>
2018-01-31 15:24:26 -0500

    post lecture

--- lib/book.rb
+++ lib/book.rb
@@ -2,22 +2,30 @@
 
 class Book
 
+  @@all = []
+
   def initialize(title, authors)
     @title = title
     @authors = authors
   end
-
 
   def title
     @title
   end
 
   def authors
-    @authors
+    @authors.map do |author|
+      author.split(' ').map { |w| w.capitalize }.join(' ')
+    end.join(' & ')
   end
 
   def to_string
-    # "Title of the Book" by Author One & Author Two
+    "\"#{title}\" by #{authors}"
+  end
+
+  # Class Methods (a method you call on the class)
+  def self.all
+    @@all
   end
 
 end
